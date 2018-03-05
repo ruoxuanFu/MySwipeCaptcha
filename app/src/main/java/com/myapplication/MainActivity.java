c2d80cad873df71b5e660e46f8e9380a9d6a0791
ruoxuan.fu <ruoxuan.fu@wheatek.com>
2018-03-05 15:55:28 +0800

    demo

--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -11,9 +11,11 @@ import com.slidingcaptcha.SlidingCaptchaView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SlidingCaptchaView mSlidingCaptchaView;
+    private SlidingCaptchaView withSeeKBar;
     private SeekBar mSeekBar;
-    private Button mChange;
+    private Button mChange1;
+    private SlidingCaptchaView withFinger;
+    private Button mChange2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +25,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        mSlidingCaptchaView = findViewById(R.id.slidingCaptchaView);
+        withSeeKBar = findViewById(R.id.withSeeKBar);
         mSeekBar = findViewById(R.id.dragBar);
-        mChange = findViewById(R.id.btnChange);
+        mChange1 = findViewById(R.id.btnChange1);
+        withFinger = findViewById(R.id.withFinger);
+        mChange2 = findViewById(R.id.btnChange2);
 
-        mChange.setOnClickListener(new View.OnClickListener() {
+        mChange1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mSlidingCaptchaView.createCaptcha();
+                withSeeKBar.createCaptcha();
                 mSeekBar.setEnabled(true);
                 mSeekBar.setProgress(0);
             }
         });
 
-        mSlidingCaptchaView.setImageDrawable(getResources().getDrawable(R.drawable.image01));
-        mSlidingCaptchaView.setSliderMode(SlidingCaptchaView.WITH_FINGER);
-        mSlidingCaptchaView.setOnCaptchaMatchListener(new SlidingCaptchaView.OnCaptchaMatchListener() {
+        mChange2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                withFinger.createCaptcha();
+            }
+        });
+
+        withSeeKBar.setImageDrawable(getResources().getDrawable(R.drawable.image01));
+        withSeeKBar.setOnCaptchaMatchListener(new SlidingCaptchaView.OnCaptchaMatchListener() {
             @Override
             public void onMatchSuccess() {
                 Toast.makeText(MainActivity.this, "＼＼" + "\\" + "\\" + "٩('ω')و//／／", Toast.LENGTH_SHORT).show();
@@ -48,24 +58,39 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onMatchFail() {
                 Toast.makeText(MainActivity.this, "(╯‵□′)╯︵┻━┻ 走你！", Toast.LENGTH_SHORT).show();
-                mSlidingCaptchaView.resetCaptcha();
+                withSeeKBar.resetCaptcha();
                 mSeekBar.setProgress(0);
             }
         });
         mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                mSlidingCaptchaView.setCurrentSwipeValue(progress);
+                withSeeKBar.setCurrentSwipeValue(progress);
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-                mSeekBar.setMax(mSlidingCaptchaView.getMaxSwipeValue());
+                mSeekBar.setMax(withSeeKBar.getMaxSwipeValue());
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                mSlidingCaptchaView.matchCaptcha();
+                withSeeKBar.matchCaptcha();
+            }
+        });
+
+        withFinger.setImageDrawable(getResources().getDrawable(R.drawable.image02));
+        withFinger.setSliderMode(SlidingCaptchaView.WITH_FINGER);
+        withFinger.setOnCaptchaMatchListener(new SlidingCaptchaView.OnCaptchaMatchListener() {
+            @Override
+            public void onMatchSuccess() {
+                Toast.makeText(MainActivity.this, "(￣▽￣)~*", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onMatchFail() {
+                Toast.makeText(MainActivity.this, " (*｀皿´*)ﾉ ", Toast.LENGTH_SHORT).show();
+                withFinger.resetCaptcha();
             }
         });
     }
